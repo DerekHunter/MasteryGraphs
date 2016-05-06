2b3b1b6c47e8a86de61c231bc5418202eae1c63f
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-05-05 20:38:10 -0600

    More stuff

--- public/app/dataController.js
+++ public/app/dataController.js
@@ -1,9 +1,36 @@
 angular.module('MasteryGraphs').controller('DataController', function($scope, ChampionService){
 
 	var DataController = this;
-	DataController.message = "Controller Message!";
-	$scope.message = ChampionService.message;
-	$scope.messageTwo = DataController.message;
 
+	$scope.staticData = ChampionService.staticChampionData;
+	$scope.leagues = ChampionService.leagues;
+
+	$scope.currentChampion = ChampionService.staticChampionData[0]
+	$scope.currentLeague = ChampionService.leagues[3];
+	$scope.searchText = ""
+
+
+	$scope.init = function(){
+		ChampionService.GetStaticChampionData();
+		$scope.staticData = ChampionService.staticChampionData;
+		ChampionService.GetChampionData($scope.currentChampion, $scope.currentLeague);
+		$scope.data =  ChampionService.graphData;
+	}
+
+	$scope.ChangeChampion = function(){
+		$scope.currentChampion = 2;
+		ChampionService.GetChampionData($scope.currentChampion, $scope.currentLeague);
+		$scope.data =  ChampionService.graphData;
+	}
+
+	$scope.ChangeLeague = function(){
+		$scope.currentLeague = ChampionService.leagues[5]
+		ChampionService.SetGraphData($scope.currentLeague);
+		$scope.data =  ChampionService.graphData;
+	}
+
+	$scope.getMatches = function(text){
+		
+	}
 
 });
--- public/app/dataService.js
+++ public/app/dataService.js
@@ -1,7 +1,49 @@
 angular.module('MasteryGraphs').factory('ChampionService', function($http){
 
 	ChampionService = {}
-	ChampionService.message = "Service message";
+	ChampionService.staticChampionData = [];
+	ChampionService.isReady = false;
+	ChampionService.leagues = ["UNRANKED", "BRONZE", "SILVER", "GOLD", "PLATINUM", "DIAMOND", "MASTER", "CHALLENGER"];
+	ChampionService.data = [];
+	ChampionService.graphData;
+
+
+	ChampionService.GetStaticChampionData = function(){
+		$http({
+			method: 'GET',
+			url: '/api/champions'
+		}).then(function successCallback(response) {
+			for(x = 0; x < response.data.length; x++){
+				ChampionService.staticChampionData.push(response.data[x]);
+			}
+			console.log(ChampionService.staticChampionData)
+			ChampionService.isReady = true;
+		}, function errorCallback(response) {
+			console.log(response);
+		});
+	}
+
+	ChampionService.GetChampionData = function(championId, league){
+		$http({
+			method: 'GET',
+			url: '/api/champion/' + championId
+		}).then(function successCallback(response) {
+			ChampionService.data = response.data
+			ChampionService.SetGraphData(league);
+		}, function errorCallback(response) {
+			console.log(response);
+		});
+	};
+
+	ChampionService.SetGraphData = function(league){
+		ChampionService.graphData = ChampionService.data.filter(function(value){
+			return league == value.league
+		})
+	}
+
+	
+
 	return ChampionService;
+	
 
 })
