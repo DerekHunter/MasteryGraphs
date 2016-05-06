744d1980d6bd5aef991f58db62cc25d41b032cbb
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-05-05 18:16:32 -0600

    Second commit

---
+++ public/app/app.js
@@ -0,0 +1,16 @@
+app = angular.module('MasteryGraphs', [
+	'ngRoute'
+]);
+
+app.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+		when('/graphs', {
+			templateUrl: 'app/partials/graphs.html',
+			controller: 'DataController'
+		}).
+		otherwise({
+			redirectTo:'/graphs'
+		});
+
+	}])
---
+++ public/app/dataController.js
@@ -0,0 +1,8 @@
+angular.module('MasteryGraphs').controller('DataController', function($scope){
+
+	var dataController = this;
+	dataController.message = "Hello fucker!";
+	$scope.message = "TEST";
+
+
+});
