f76a0bb262634a42caabaa7ab2ea0d57e10543f5
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-05-08 17:16:58 -0600

    Fixing some localhoststuff

--- data_pipeline/crawler.js
+++ data_pipeline/crawler.js
@@ -12,7 +12,7 @@ featuredGame = JSON.parse(request('GET', "https://na.api.pvp.net/observer-mode/r
 for(var gameIndex = 0; gameIndex < featuredGame.gameList.length; gameIndex++){
 	for(var participantsIndex = 0; participantsIndex < featuredGame.gameList[gameIndex].participants.length; participantsIndex++){
 		console.log("Inserting " + featuredGame.gameList[gameIndex].participants[participantsIndex].summonerName)
-		request('POST', "http://192.168.1.138:3000/api/summoner", {
+		request('POST', "http://localhost:3000/api/summoner", {
 			json:{
 				"summonerName":featuredGame.gameList[gameIndex].participants[participantsIndex].summonerName.toLowerCase().replace(/ /g,''),
 			}
@@ -22,7 +22,7 @@ for(var gameIndex = 0; gameIndex < featuredGame.gameList.length; gameIndex++){
 
 while(true){
 	try{
-		summoner = JSON.parse(request('GET', "http://192.168.1.138:3000/api/next/crawl").body);
+		summoner = JSON.parse(request('GET', "localhost:3000/api/next/crawl").body);
 		summonerName = summoner.summonerName.toLowerCase().replace(/ /g,'')
 		id = JSON.parse(request('GET', 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summonerName + '?api_key=' + apiKey).body)[summonerName].id;
 		matchHistory = JSON.parse(request('GET', "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/" + id + "?api_key="+apiKey).body)['matches']	
@@ -30,7 +30,7 @@ while(true){
 			matchData = JSON.parse(request('GET', "https://na.api.pvp.net/api/lol/na/v2.2/match/"+matchHistory[gameCnt].matchId+"?api_key="+apiKey).body)
 			for(var participantsIndex = 0; participantsIndex < matchData.participantIdentities.length; participantsIndex++){
 				console.log("Inserting " + matchData.participantIdentities[participantsIndex].player.summonerName)
-				request('POST', "http://192.168.1.138:3000/api/summoner", {
+				request('POST', "http://localhost:3000/api/summoner", {
 					json:{
 						"summonerName":matchData.participantIdentities[participantsIndex].player.summonerName.toLowerCase().replace(/ /g,''),
 					}
