25fe84f702636eddad9558ab6a7c8ff26b78427e
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-05-08 17:18:53 -0600

    more localhost shit

--- data_pipeline/processor.js
+++ data_pipeline/processor.js
@@ -7,16 +7,16 @@ if (apiKey == ''){
 	console.log("Not a valid api key");
 }
 
-summoner = JSON.parse(request('GET', "http://192.168.1.138:3000/api/next/process").body);
+summoner = JSON.parse(request('GET', "localhost:3000/api/next/process").body);
 while(summoner !=null){
 	if(!summoner.processed){
 		// sleep.usleep(500000);
 		console.log("Processing " + summoner.summonerName);
-		request('POST', "http://192.168.1.138:3000/api/process", {
+		request('POST', "localhost:3000/api/process", {
 			json:{
 				"summonerName":summoner.summonerName,
 			}
 		})
 	}
-	summoner = JSON.parse(request('GET', "http://192.168.1.138:3000/api/next/process").body);
+	summoner = JSON.parse(request('GET', "localhost:3000/api/next/process").body);
 }
