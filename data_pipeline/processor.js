da4234977c63e87334c8d24c66e6ab2c7627dc25
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-05-08 17:28:32 -0600

    Last one?

--- data_pipeline/processor.js
+++ data_pipeline/processor.js
@@ -6,15 +6,15 @@ if (apiKey == ''){
 	console.log("Not a valid api key");
 }
 
-summoner = JSON.parse(request('GET', "localhost:3000/api/next/process").body);
+summoner = JSON.parse(request('GET', "http://localhost:3000/api/next/process").body);
 while(summoner !=null){
 	if(!summoner.processed){
 		console.log("Processing " + summoner.summonerName);
-		request('POST', "localhost:3000/api/process", {
+		request('POST', "http://localhost:3000/api/process", {
 			json:{
 				"summonerName":summoner.summonerName,
 			}
 		})
 	}
-	summoner = JSON.parse(request('GET', "localhost:3000/api/next/process").body);
+	summoner = JSON.parse(request('GET', "http://localhost:3000/api/next/process").body);
 }
