3f7cdaae5b30603498bef12292f44b310f46922c
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-05-09 20:33:47 -0600

    More UI Stuff

--- public/app/recommenderController.js
+++ public/app/recommenderController.js
@@ -10,13 +10,36 @@ angular.module('MasteryGraphs').controller('RecommenderController', function($sc
 		RecommenderController.currentChampion = RecommenderService.staticChampionData[0]
 		RecommenderController.currentChampionId = RecommenderService.staticChampionData[0].id;
 		RecommenderController.currentChampionName = RecommenderService.staticChampionData[0].name;
-		RecommenderController.image = "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/Aatrox.png "
+		RecommenderController.image = "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/Aatrox.png"
 	});
 
 	RecommenderService.GetRecommenderData(1, function(){
 		console.log(RecommenderService.recommenderData);
 	})
 
+
+	$scope.$watch('ctrl.currentChampionName', function(newValue, oldValue){
+		if(RecommenderController.staticData){
+			console.log("champion changed");
+			RecommenderController.currentChampion = RecommenderController.staticData.find(function(obj){
+				return obj.name == newValue;
+			})
+			RecommenderService.GetRecommenderData(RecommenderController.currentChampion.id, function(){
+				console.log("UPDAT REC LIST");
+				RecommenderController.image = "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/"+RecommenderController.currentChampion.name+".png"
+				RecommenderController.reconChamps = [];
+				for(x = 1; x < 4; x++){
+					RecommenderController.reconChamps.push(RecommenderController.currentChampion = RecommenderController.staticData.find(function(obj){
+						return obj.id == RecommenderService.recommenderData[x].champId;
+					}))
+					RecommenderController.reconChamps[x-1].image = "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/"+RecommenderController.reconChamps[x-1].name+".png"
+				}
+
+				console.log(RecommenderController.reconChamps);
+			})
+		}
+		
+	})
 
 
 
