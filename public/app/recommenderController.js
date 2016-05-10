dbc71751a6ed75570398b4b53a608d584bc7d2ce
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-05-09 20:16:38 -0600

    more layout stuff getting close to being done

--- public/app/recommenderController.js
+++ public/app/recommenderController.js
@@ -6,7 +6,11 @@ angular.module('MasteryGraphs').controller('RecommenderController', function($sc
 
 
 	RecommenderService.GetChampionData(function(){
-
+		RecommenderController.staticData = RecommenderService.staticChampionData;
+		RecommenderController.currentChampion = RecommenderService.staticChampionData[0]
+		RecommenderController.currentChampionId = RecommenderService.staticChampionData[0].id;
+		RecommenderController.currentChampionName = RecommenderService.staticChampionData[0].name;
+		RecommenderController.image = "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/Aatrox.png "
 	});
 
 	RecommenderService.GetRecommenderData(1, function(){
